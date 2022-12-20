6518845b87343e71ade0b59b30f0026e42940c22
Yulia <rosiely@mail.ru>
2022-12-20 08:25:33 +0500

    Lesson 4

---
+++ Task_1.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+ 
+// Программа для обращения к ArrayList
+public class Task_1 {
+    public static<T> List<T> reverseList(List<T> list)
+    {
+        // Делаем копию и переворачиваем
+        List<T> reverse = new ArrayList<>(list);
+        Collections.reverse(reverse);
+        return reverse;
+    }
+ 
+    public static void main(String[] args)
+    {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+ 
+        List<Integer> reverse = reverseList(list);
+        System.out.println(reverse);
+    }
+}
